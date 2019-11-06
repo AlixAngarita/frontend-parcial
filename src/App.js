55e70986ef7092c5553557072e80f1a7ac544d53
Alix <andreangarita1@hotmail.com>
2019-11-06 14:25:30 -0500

    small fixes 2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import store from "./store";
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import PrivateRoute from "./components/private-route/PrivateRoute";
 import Dashboard from "./components/dashboard/Dashboard";
@@ -42,7 +41,6 @@ class App extends Component {
           <div className="App">
             <Navbar />
             <Route exact path="/" component={Landing} />
-            <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
             <Switch>
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -14,20 +14,7 @@ class Landing extends Component {
               Parcial 2 Web
             </p>
             <br />
-            <div className="col s6">
-              <Link
-                to="/register"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px"
-                }}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-              >
-                Register
-              </Link>
-            </div>
-            <div className="col s6">
+            <div className="col s12">
               <Link
                 to="/login"
                 style={{
@@ -35,7 +22,7 @@ class Landing extends Component {
                   borderRadius: "3px",
                   letterSpacing: "1.5px"
                 }}
-                className="btn btn-large btn-flat waves-effect white black-text"
+                className="btn btn-large btn-flat waves-effect blue accent-3 white-text"
               >
                 Log In
               </Link>
